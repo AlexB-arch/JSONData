8275579a7136d6a64d783b50861eb387c429eaf7
Alexander Burgos <67289166+AlexB-arch@users.noreply.github.com>
2022-10-25 21:24:08 -0500

    Adds functionality to add user to database

--- app/src/main/java/com/example/somedata/MainActivity.java
+++ app/src/main/java/com/example/somedata/MainActivity.java
@@ -4,65 +4,93 @@ import static com.google.firebase.database.FirebaseDatabase.getInstance;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    // Firebase
     private DatabaseReference databaseReference;
-    private Button search;
-    private EditText searchUser;
+    // Firebase URL
+    private static final String FIREBASE_URL = "https://sample-data-app-default-rtdb.firebaseio.com";
+
+    // Widgets
+    private EditText mEditText;
+    private Button mButton;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseReference = getInstance().getReference(getResources().getString(R.string.json_url));
+        databaseReference = getInstance().getReference();
 
-        searchUser = findViewById(R.id.writeText);
-        search = findViewById(R.id.search_button);
+        mEditText = findViewById(R.id.writeText);
+        mButton = findViewById(R.id.search_button);
+        mTextView = findViewById(R.id.readText);
 
-        search.setOnClickListener((View.OnClickListener) readUser(String.valueOf(searchUser)));
+        // Search button
+        mButton.setOnClickListener(v -> {
+            String text = mEditText.getText().toString();
+            if (!text.isEmpty()) {
+                // Search for the text
+                search(text);
+            }
+        });
 
+        // Add user button
+        findViewById(R.id.addUserButton).setOnClickListener(v -> {
+            // Create a new user
+            User user = new User("John", "email address");
+            // Add the user to the database
+            databaseReference.child("users").child("1").setValue(user);
+        });
     }
 
-    public void writeNewUser(String userId, String name, String email){
-        User user = new User(name, email);
+    // Search for the specified text
+    private void search(String text) {
+        // Search for the text
+        databaseReference.child("users").equalTo(text).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // Get the data
+                DataSnapshot dataSnapshot = task.getResult();
+                // Check if the data exists
+                if (dataSnapshot.exists()) {
+                    // Get the data
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        // Get the data
+                        User user = snapshot.getValue(User.class);
+                        // Check if the data is not null
+                        if (user != null) {
+                            // Get the data
+                            String username = user.username;
+                            String email = user.email;
+                            // Log the data
+                            Log.d(TAG, "search: username: " + username);
+                            Log.d(TAG, "search: email: " + email);
 
-        databaseReference.child("users").child(userId).setValue(user);
-    }
-
-    public CharSequence readUser(String userId){
-        databaseReference.child("users").child(userId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()){
-                    Log.e("firebase", "Error getting data.", task.getException());
-                }
-                else {
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                            // Display the data
+                            mTextView.setText("username: " + username + " email: " + email);
+                        }
+                    }
                 }
             }
         });
-        return (CharSequence) databaseReference.child("users").child(userId).get();
     }
 
-    public void updateUser(String userId, String newName){
-        databaseReference.child("users").child(userId).child("name").setValue(newName);
-    }
-
-    public void deleteUser(String userId){
-        databaseReference.child("users").child(userId).removeValue();
+    // Adds a new user to the database
+    private void addUser() {
+        // Create a new user
+        User user = new User("John", "myemail");
+        // Add the user to the database
+        databaseReference.child("users").push().setValue(user);
     }
 }
